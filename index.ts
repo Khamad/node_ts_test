c77863c8aa8236cdd755d7e9006999dc65d8b2ce
Hamad <g.osmanov.muhammad@gmail.com>
2018-02-02 20:10:36 +0300

    Add server code

--- index.ts
+++ index.ts
@@ -3,3 +3,13 @@ console.log('Huge change happened...')
 console.warn('DEPLOYINGGGG...')
 console.log('Server pretends to be working...')
 console.warn('Trying...')
+
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT
+
+app.get('/', (req, res) => res.send('Working...'))
+
+app.listen(PORT)
+
